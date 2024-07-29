55b5e13f90608d3733f55526e32aa1eb6d1bb1a4
answer35 <rbanswer35@gmail.com>
2024-07-30 00:34:58 +0200

    sm8250-common: Include some LineageOS apps

--- common.mk
+++ common.mk
@@ -30,6 +30,13 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Apps
+PRODUCT_PACKAGES += \
+    Aperture \
+    Etar \
+    Glimpse \
+    Recorder
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
